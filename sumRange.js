90857920d29964661dadf7af7267e35743ff9949
Stephen Done <srdone@gmail.com>
2015-01-15 06:38:15 -0700

    Changed sum name to range

--- sumRange.js
+++ sumRange.js
@@ -1,7 +1,7 @@
 // Write a range function that takes two arguments (start and end)
 // It should return the sum of all numbers froms tart to end (inclusive)
 
-var sum = function(start, end) {
+var range = function(start, end) {
   var total = 0;
   for (var i = start; i <= end; i++) {
     total += i;
@@ -10,5 +10,5 @@ var sum = function(start, end) {
 };
 
 //test
-console.log('sum(1,2): ' + sum(1,2));
-console.log('sum(-1,1): ' + sum(-1,1));
+console.log('range(1,2): ' + range(1,2));
+console.log('range(-1,1): ' + range(-1,1));
