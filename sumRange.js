43a10ff16b4ae0fdaea98daa81138de0536c3f5f
Stephen Done <srdone@gmail.com>
2015-01-15 06:52:33 -0700

    separate sum into sum and range functions

--- sumRange.js
+++ sumRange.js
@@ -1,14 +1,30 @@
 // Write a range function that takes two arguments (start and end)
-// It should return the sum of all numbers froms tart to end (inclusive)
+// It should return an array of all numbers froms start to end (inclusive)
 
 var range = function(start, end) {
-  var total = 0;
+  var result = [];
   for (var i = start; i <= end; i++) {
-    total += i;
+    result.push(i);
   }
-  return total;
+  return result;
 };
 
 //test
 console.log('range(1,2): ' + range(1,2));
 console.log('range(-1,1): ' + range(-1,1));
+console.log('range(1,10): ' + range(1,10));
+
+//Write a sum function that takes an array of numbers and returns the sum
+
+var sum = function(arr) {
+  var total = 0;
+  for (var i in arr) {
+    // test each element to make sure it is part of arr and not arr's prototype
+    // make sure to acces each element using the obj[propertyName] structure
+    if (arr.hasOwnProperty(i)) total += arr[i];
+  }
+  return total;
+};
+
+//test
+console.log('sum(range(1, 10)): ' + sum(range(1, 10)));
