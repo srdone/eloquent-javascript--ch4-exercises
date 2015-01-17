451c7c4b05075f2c508116344d6c1abaece9eef6
Stephen Done <srdone@gmail.com>
2015-01-17 06:27:49 -0700

    completed nth - recursive

--- aList.js
+++ aList.js
@@ -42,3 +42,22 @@ var prepend = function(element, list) {
 
 var ans3 = prepend(5, ans1);
 console.log(ans3);
+
+var nth = function(list, idx) {
+  if (idx < 0 || (list.rest === null && idx > 0)) {
+    // there aren't any elements at indexes less than zero so return undefined
+    // if we gave an index that didn't exist (too high), we will reach a null
+    // list.rest, but the idx will still be greater than 0 - return undefined.
+    return undefined;
+  } else if (idx === 0) {
+    // when idx === 0, we have reached the element we want
+    return list.value;
+  } else {
+    // if idx is > 0, we use recursion to keep going deeper.
+    // remember to subtract 1 from the index and give the rest as the new list
+    return nth(list.rest, idx - 1);
+  }
+};
+
+var ans4 = nth(arrayToList([10, 20, 30]), 1);
+console.log(ans4);
